5470be646bcf250f636f0d20783fdbd93445b521
Daniel <danielhoang79@gmail.com>
2020-03-16 00:49:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const today = new Date().toDateString();
-//   const timeString = ('0' + today.getDate()).slice(-2) + '/'
-//                    + ('0' + (today.getMonth()+1)).slice(-2) + '/'
-//                    + today.getFullYear();
-  core.setOutput("today", today);
+  const today = new Date();
+  const timeString = ('0' + today.getDate()).slice(-2) + '/'
+                   + ('0' + (today.getMonth()+1)).slice(-2) + '/'
+                   + today.getFullYear();
+  core.setOutput("today", timeString);
 } catch (error) {
   core.setFailed(error.message);
 }
